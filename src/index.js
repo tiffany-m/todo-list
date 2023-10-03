b04ab7a4fb67a093927a7f9b53862ff3f6702799
Tiffany M <67019104+tiffany-m@users.noreply.github.co>
2023-10-03 15:09:06 -0400

    Add image loading to webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './style.css';
+import Icon from './images/icon.png';
 
 console.log("working")
+
+const hello = document.querySelector(".hello");
+
+const myIcon = new Image();
+myIcon.src = Icon;
+
+
+hello.appendChild(myIcon);
