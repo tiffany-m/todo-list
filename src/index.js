77207ab2dbd6577e84f7496ad076c81d8559c5e5
Tiffany M <67019104+tiffany-m@users.noreply.github.co>
2023-10-12 15:12:21 -0400

    Add DOM and other variables

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
-import './style.css';
-import './myFont.ttf'
-import Icon from './images/icon.png';
+const toDoProjectContainer = document.getElementById("to-do-project-container");
+const addNewProjectBtn = document.getElementById("add-new-project-btn");
+const clearBtn = document.getElementById("clear-btn");
+const submitBtn = document.getElementById("submit-btn");
+const closeModalBtn = document.getElementById("close-btn");
+const form = {
+    title: document.getElementById("title"),
+    description: document.getElementById("description"),
+    dueDate: document.getElementById("due-date"),
+    priority: document.getElementById("priority")
+}
+const priorityBtn = {
+    yellow: document.getElementById('low-priority-btn'),
+    orange: document.getElementById('medium-priority-btn'),
+    red: document.getElementById('high-priority-btn')
+}
+let selectedPriority = "";
+let index;
+const indexFromStorage = localStorage.getItem("index");
 
-console.log("working")
+// Initialize localStorageProjects from local storage if data exists.
+// This prevents data loss when the page is refreshed.
+let localStorageProjects = JSON.parse(localStorage.getItem("myProjects"));
 
-const hello = document.querySelector(".hello");
-
-const myIcon = new Image();
-myIcon.src = Icon;
-
-
-hello.appendChild(myIcon);
+// defaults to prevent errors
+if (localStorageProjects == null) localStorageProjects = [];
+if (indexFromStorage && !isNaN(indexFromStorage)) {
+    index = JSON.parse(indexFromStorage);
+} else {
+    index = 0;
+}
